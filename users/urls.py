43f14d1e948414ac8f118e48bae188444eff55cc
joy-amorin <amorinkaren2021@gmail.com>
2025-08-07 17:58:39 -0300

    add user registration, JWT authentication and login endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('', views.test_api),
-    path('/register/',views.register_user, name='register')
+    path('/register/', views.register_user, name='register'),
+    path('/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #login
+    path('/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  #renew  token
 ]
--- users/views.py
+++ users/views.py
@@ -20,4 +20,4 @@ def register_user(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
